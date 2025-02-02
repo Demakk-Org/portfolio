ced6ba6ee6372b942f09631184b4a8a540277175
ermias alehegn <ermiaseat1987@gmail.com>
2025-02-02 12:46:08 +0300

    deploy server to cpanel

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ import { parse } from "url";
 import next from "next";
 
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+const hostname = "localhost";
+const port = process.env.PORT || 3000;
+const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(process.env.PORT || 3000, (err) => {
+  }).listen(port, (err) => {
     if (err) throw err;
-    console.log("> Ready on http://localhost:3000");
+    console.log(`> Ready on http://${hostname}:${port}`);
   });
 });
